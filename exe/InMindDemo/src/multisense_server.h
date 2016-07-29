b83c545281d8bca10a94b4fc915fcd776b5bf825
Chirag Raman <chirag.raman@gmail.com>
2016-07-29 18:07:31 -0400

    Add multisense_server skeleton source files

---
+++ exe/InMindDemo/src/multisense_server.h
@@ -0,0 +1,16 @@
+/**
+* @Author: Chirag Raman <chirag>
+* @Date:   2016-07-29T14:35:48-04:00
+* @Email:  chirag.raman@gmail.com
+* @Last modified by:   chirag
+* @Last modified time: 2016-07-29T14:36:52-04:00
+* @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
+*/
+
+
+#ifndef EXE_INMINDDEMO_MULTISENSE_SERVER_H_
+#define EXE_INMINDDEMO_MULTISENSE_SERVER_H_
+
+
+
+#endif /* end of include guard: EXE_INMINDDEMO_MULTISENSE_SERVER_H_ */
