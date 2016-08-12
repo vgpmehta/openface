d78cf1c65c207f843f649f640876b55364ab5548
Chirag Raman <chirag.raman@gmail.com>
2016-08-12 16:39:33 -0400

    turn camera test off

--- exe/InMindDemo/src/main.cpp
+++ exe/InMindDemo/src/main.cpp
@@ -3,7 +3,7 @@
 * @Date:   2016-05-09T21:14:02-04:00
 * @Email:  chirag.raman@gmail.com
 * @Last modified by:   chirag
-* @Last modified time: 2016-08-12T12:30:49-04:00
+* @Last modified time: 2016-08-12T16:39:13-04:00
 * @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
 */
 
@@ -28,7 +28,7 @@ using namespace InmindDemo;
 
 // Use this to run from the camera with a device id like "/dev/video0" instead
 // of grabbing from an RTSP stream
-#define CAMERA_TEST (1)
+#define CAMERA_TEST (0)
 
 /********
  * HELPERS
