e4543e322381c1cc7ac0ab8bb3901983ec7b74ec
Pingfeng Luo <luopingfeng@cvte.com>
2017-06-27 18:27:32 +0800

    add python tracking interface

--- python/pyopenface.cpp
+++ python/pyopenface.cpp
@@ -4,6 +4,7 @@
 #include <numpy/arrayobject.h>
 #include <opencv2/core.hpp>
 #include "detector.hpp"
+#include "tracker.hpp"
 
 namespace bp = boost::python;
 
@@ -13,7 +14,10 @@ bp::object landmark(Detector *detector, bp::object frame_obj, const bp::list& re
   PyArrayObject* frame_arr = reinterpret_cast<PyArrayObject*>(frame_obj.ptr());
   const int height = PyArray_DIMS(frame_arr)[0];
   const int width = PyArray_DIMS(frame_arr)[1];
-  cv::Mat frame(cv::Size(width, height), CV_8UC1, PyArray_DATA(frame_arr), cv::Mat::AUTO_STEP);
+  cv::Mat frame(cv::Size(width, height), 
+		  CV_8UC1, 
+		  PyArray_DATA(frame_arr), 
+		  cv::Mat::AUTO_STEP);
 
   //rect_object[left, top, right, bottom]
   bp::ssize_t rect_len = bp::len(rect_object);
@@ -22,16 +26,22 @@ bp::object landmark(Detector *detector, bp::object frame_obj, const bp::list& re
   double rect_y = bp::extract<double>(rect_object[1]);
   double rect_height = bp::extract<double>(rect_object[2]) - bp::extract<double>(rect_object[0]);
   double rect_width  = bp::extract<double>(rect_object[3]) - bp::extract<double>(rect_object[1]);
-  cv::Rect_<double> face_rect(rect_x, rect_y, rect_height, rect_width);
+  cv::Rect_<double> face_rect( rect_x, 
+		  rect_y, 
+		  rect_height, 
+		  rect_width);
 
-  cv::Mat_<double> landmarks = detector->Run(frame, face_rect);
+  cv::Mat_<double> face_landmarks = detector->Run(frame, face_rect);
 
-  long int landmarks_size[2] = {landmarks.rows, landmarks.cols};
-  PyObject * landmarks_obj = PyArray_SimpleNewFromData( 2, landmarks_size, NPY_DOUBLE, landmarks.data);
-  bp::handle<> landmarks_handle(landmarks_obj);
-  bp::numeric::array landmarks_arr(landmarks_handle);
+  long int face_landmarks_size[2] = {face_landmarks.rows, face_landmarks.cols};
+  PyObject * face_landmarks_obj = PyArray_SimpleNewFromData( 2, 
+		  face_landmarks_size, 
+		  NPY_DOUBLE, 
+		  face_landmarks.data);
+  bp::handle<> face_landmarks_handle(face_landmarks_obj);
+  bp::numeric::array face_landmarks_arr(face_landmarks_handle);
 
-  return landmarks_arr.copy();
+  return face_landmarks_arr.copy();
 
 }
 
@@ -41,8 +51,13 @@ bp::list detect(Detector *detector, bp::object frame_obj)
     PyArrayObject *frame_arr = reinterpret_cast<PyArrayObject *>(frame_obj.ptr());
     const int height = PyArray_DIMS(frame_arr)[0];
     const int width = PyArray_DIMS(frame_arr)[1];
-    cv::Mat grayscale_frame(cv::Size(width, height), CV_8UC1, PyArray_DATA(frame_arr), cv::Mat::AUTO_STEP);
+    cv::Mat grayscale_frame(cv::Size(width, height), 
+		    CV_8UC1, 
+		    PyArray_DATA(frame_arr), 
+		    cv::Mat::AUTO_STEP);
+
     cv::Rect_<double> face_rect = detector->DetectFace(grayscale_frame);
+
     bp::list list;
     list.append(face_rect.x);
     list.append(face_rect.y);
@@ -50,6 +65,54 @@ bp::list detect(Detector *detector, bp::object frame_obj)
     list.append(face_rect.y+face_rect.width);
     //return list is face_rect[left, top, right, bottom]
     return list;
+
+}
+
+Tracker* init_tracker()
+{
+
+  vector<string> args;
+  args.push_back(string(" ")); //for low level c++ access args[0]
+  return new Tracker(args);
+
+}
+
+bp::object tracking(Tracker *tracker, bp::object frame_obj)
+{
+
+    PyArrayObject *frame_arr = reinterpret_cast<PyArrayObject *>(frame_obj.ptr());
+    const int height = PyArray_DIMS(frame_arr)[0];
+    const int width = PyArray_DIMS(frame_arr)[1];
+    cv::Mat grayscale_frame(cv::Size(width, height), CV_8UC1, PyArray_DATA(frame_arr), cv::Mat::AUTO_STEP);
+
+    vector<cv::Rect_<double> > face_rects;
+    vector<cv::Mat_<double> > face_landmarks;
+    bool success = tracker->tracking(grayscale_frame, 
+		    face_rects, 
+		    face_landmarks);
+
+    bp::list face_landmarks_list;
+    for (int i = 0; i < face_landmarks.size(); i++)
+    {
+      long int landmarks_size[2] = {face_landmarks[i].rows, face_landmarks[i].cols};
+      PyObject *landmarks_obj = PyArray_SimpleNewFromData(2,
+                                                          landmarks_size,
+                                                          NPY_DOUBLE,
+                                                          face_landmarks[i].data);
+      bp::handle<> landmarks_handle(landmarks_obj);
+      bp::list landmarks_arr(landmarks_handle);
+      face_landmarks_list.extend(landmarks_arr);
+    }
+
+    bp::list face_rects_list;
+    for(int i=0; i < face_rects.size(); i++){
+      face_rects_list.append(face_rects[i].x);
+      face_rects_list.append(face_rects[i].y);
+      face_rects_list.append(face_rects[i].x + face_rects[i].height);
+      face_rects_list.append(face_rects[i].y + face_rects[i].width);
+    }
+
+    return bp::make_tuple(face_rects_list, face_landmarks_list);
 
 }
 
@@ -62,6 +125,11 @@ BOOST_PYTHON_MODULE(pyopenface) {
       .def("__init__", bp::make_constructor(&Detector::Create))
       .def("detect",  &detect)
       .def("landmark", &landmark)
+      ;
+
+  bp::class_<Tracker>("Tracker", bp::no_init)
+      .def("__init__", bp::make_constructor(&init_tracker))
+      .def("tracking",  &tracking)
       ;
 
 }
--- python/test.py
+++ python/test.py
@@ -6,23 +6,54 @@ import time
 base_path = os.path.realpath(__file__)
 base_path = base_path[:base_path.find('OpenFace')]
 
+debug = True
+
 def main():
-    of = pyopenface.Detector(base_path + "OpenFace/lib/local/LandmarkDetector/model/main_clnf_general.txt")
+    detector = pyopenface.Detector(base_path + "OpenFace/lib/local/LandmarkDetector/model/main_clnf_general.txt")
+    tracker = pyopenface.Tracker()
     img = cv2.imread(base_path + "OpenFace/samples/sample3.jpg")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_for_track = img.copy()
     time_0 = time.time()
-    max_face_rect = of.detect(img)
+    face_rects_max = detector.detect(img)
     #max_face_rect is a rectangle [left, top, right , bottom]
     time_1 = time.time()
     print "detect take {:.4f} Second".format(time_1-time_0)
-    re = of.landmark(img, max_face_rect)
+    re = detector.landmark(img, face_rects_max)
+    face_keypoints = [(int(kp[0]), int(kp[1])) for kp in zip(re[0], re[1])]
     time_2 = time.time()
     print "landmark take {:.4f} Second".format(time_2-time_1)
-    keypoints = [(int(kp[0]), int(kp[1])) for kp in zip(re[0], re[1])]
-    cv2.rectangle(img, (int(max_face_rect[0]),int(max_face_rect[1])), (int(max_face_rect[2]), int(max_face_rect[3])),(255, 0, 0), 2)
-    for kp in keypoints:
-       cv2.circle(img, kp, 2 , (255, 0, 0), 1)
-    cv2.imwrite("result.jpg", img)
+    for kp in face_keypoints:
+        cv2.circle(img, kp, 2 , (255, 0, 0), 1)
+    cv2.rectangle(img,
+                  (int(face_rects_max[0]), int(face_rects_max[1])),
+                  (int(face_rects_max[2]), int(face_rects_max[3])),
+                  (255, 0, 0),
+                  2)
+    if debug:
+        cv2.imwrite("detect_result.jpg", img)
+
+    time_2 = time.time()
+    tr_results = tracker.tracking(img_for_track)
+    time_3 = time.time()
+    print "tracking take {:.4f} Second".format(time_3-time_2)
+
+    face_rects = tr_results[0]
+    face_keypoints  = tr_results[1]
+    face_keypoints = [(int(kp[0]), int(kp[1])) for kp in zip(face_keypoints[0], face_keypoints[1])]
+    num_face = len(face_keypoints)/68
+    for kp in face_keypoints:
+        cv2.circle(img_for_track, kp, 2 , (255, 0, 0), 1)
+    for i in range(num_face):
+        cv2.rectangle(img_for_track,
+                      (int(face_rects[i*4+0]), int(face_rects[i*4+1])),
+                      (int(face_rects[i*4+2]), int(face_rects[i*4+3])),
+                      (255, 0, 0),
+                      2)
+
+    if debug:
+        cv2.imwrite("track_result.jpg", img_for_track)
+
 
 if __name__ == "__main__":
     main()
