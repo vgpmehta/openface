02f572bd479d2603947ec48760afbaea65dc408e
Philippe Gauthier <philippe.gauthier@upmc.fr>
2020-02-24 18:50:39 +0100

    Fixes #506, only adds camera with positive width and height

--- gui/OpenFaceOffline/UI_items/CameraSelection.xaml.cs
+++ gui/OpenFaceOffline/UI_items/CameraSelection.xaml.cs
@@ -49,8 +49,15 @@ namespace OpenFaceOffline
             {
                 String root = AppDomain.CurrentDomain.BaseDirectory;
                 //cams = CameraInterop.Capture.GetCameras(root);
-                cams = UtilitiesOF.SequenceReader.GetCameras(root);
-            }
+                List<Tuple<int, String, List<Tuple<int, int>>, OpenCVWrappers.RawImage>> camsTmp;
+                camsTmp = UtilitiesOF.SequenceReader.GetCameras(root);
+                cams = new List<Tuple<int, String, List<Tuple<int, int>>, OpenCVWrappers.RawImage>>();
+                foreach (var s in camsTmp)
+                {                   
+                    if ( s.Item4.Width > 0 && s.Item4.Height > 0)
+                        cams.Add(s);
+                }
+            }           
 
             int i = 0;
 
@@ -62,7 +69,8 @@ namespace OpenFaceOffline
 
             foreach (var s in cams)
             {
-
+                /*if (s.Item4.Width <= 0 || s.Item4.Height <= 0)
+                    continue;*/
                 var b = s.Item4.CreateWriteableBitmap();
                 s.Item4.UpdateWriteableBitmap(b);
                 b.Freeze();
