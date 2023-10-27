4501ab197f2cc0bea8f2eb26a945a440040fe125
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-10-28 00:39:24 +0200

    updated logs to ms

--- exe/AUExtractor/AUExtractor.cpp
+++ exe/AUExtractor/AUExtractor.cpp
@@ -265,7 +265,7 @@ int main(int argc, char **argv)
     cv::cvtColor(rgb_image, greyScale_image, cv::COLOR_BGR2GRAY);
 
     gettimeofday(&stop, NULL);
-    printf("decode: %lu\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    printf("Decode: %f\n", (double) ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000);
 
     // Detect ROI
     if (roi.width == 0)
@@ -296,7 +296,7 @@ int main(int argc, char **argv)
     bool landmark_detection_success = LandmarkDetector::DetectLandmarksInVideo(rgb_image_roi, face_model, det_parameters, greyScale_image_roi);
 
     gettimeofday(&stop, NULL);
-    printf("DetectLandmarksInVideo: %lu\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    printf("DetectLandmarksInVideo: %f\n", (double) ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000);
 
     string json;
     if (landmark_detection_success)
@@ -311,7 +311,7 @@ int main(int argc, char **argv)
       json = convertToJSON(roi, aus_intensity, aus_presence);
 
       gettimeofday(&stop, NULL);
-      printf("PredictStaticAUsAndComputeFeatures & convertToJSON: %lu\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+      printf("PredictStaticAUsAndComputeFeatures & convertToJSON: %f\n", (double)((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000);
     }
     else
     {
@@ -334,7 +334,7 @@ int main(int argc, char **argv)
     }
 
     gettimeofday(&stop_all, NULL);
-    printf("[%d] All: %lu\n\n", frame_count, (stop_all.tv_sec - start_all.tv_sec) * 1000000 + stop_all.tv_usec - start_all.tv_usec);
+    printf("[%d] All: %f\n\n", frame_count, (double) ((stop_all.tv_sec - start_all.tv_sec) * 1000000 + stop_all.tv_usec - start_all.tv_usec) / 1000);
 
     // Send reply back to client
     zmq::message_t reply(json.length());
